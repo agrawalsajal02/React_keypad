88f2e77e9a9a47f0cb3fefd357543c22f284dbac
sajalagrawal14 <sajal.agarwal703@gmail.com>
2020-06-08 06:02:11 +0530

    formated code -flexbox -styled component

---
+++ src/Components/Button.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react'
+import keys from "./keys";
+
+export default class Button extends Component {
+    render() {
+        return (
+            <div style={{ "display": "flex","justify-content":"space-around","flexWrap":"wrap",padding:"20px"}}>
+            {Object.entries(keys).map((entry) => (
+              <div key={entry[0]}  className="mt-2 mb-2 ml-2 mr-2" style={{flexBasis: "25%"}} >
+                <button
+                  className="btn btn-primary btn-xg btn-block"
+                  onTouchEnd={this.props.mouseup.bind(this,entry[0])}
+                  onMouseDown={this.props.mousedown.bind(this,entry[0])}
+                  onMouseUp={this.props.mouseup.bind(this,entry[0])}
+                >
+                  <div>{entry.map((item) => item)}</div>
+                </button>
+              </div>
+            ))}
+            <button
+              className="btn btn-primary"
+              style={{flexBasis: "25%"}}
+              key="10"
+              onClick={
+                this.props}
+            >
+              <div>back</div>
+            </button>
+
+            <button
+              className="btn btn-primary"
+              key="11"
+              style={{flexBasis: "25%"}}
+              onClick={
+                    this.props.clear}
+            >
+              <div>Clear</div>
+            </button>
+          </div>)
+    }
+}
--- src/Components/Keypad.js
+++ src/Components/Keypad.js
@@ -1,201 +1,194 @@
 import React, { Component } from "react";
-
-var keys = {
-  "1": Number,
-  "2": ["a", "b", "c"],
-  "3": ["d", "e", "f"],
-  "4": ["g", "h", "i"],
-  "5": ["j", "k", "l"],
-  "6": ["m", "n", "o"],
-  "7": ["p", "q", "r", "s"],
-  "8": ["t", "u", "v"],
-  "9": ["w", "x", "y", "z"],
-  "0": Number,
-};
+import keys from "./keys";
+import styled from 'styled-components';
+import Button from "./Button"
 export default class Keypad extends Component {
-  constructor(props) {
-    // keys.map((items)=>(console.log(items)))
-    super(props);
-    this.state = {
-      text: "",
-      id: 1,
-      no: 0,
-      timerid: null,
-      date: Date,
-    };
-
-    this.handleButtonPress = this.handleButtonPress.bind(this);
-    this.handleButtonRelease = this.handleButtonRelease.bind(this);
-  }
-  handleButtonPress = () => {
-    setTimeout(() => {}, 2000);
-    clearTimeout(this.state.timerid);
-    var d = new Date();
-    var n = d.getTime();
-    this.setState({ ...this.state, date: n });
+  
+  
+    state = {
+    text: "" /* text displaying on screen */,
+    key_id: -1 /* key position */,
+    arr_no: 0 /* alphabet in perticular key position */,
+    timerid: null /*  Previous timer Running  */,
+    Time_stamp: Date /*  time stamp of on mouse down */,
   };
 
+  /*   on Mouse down event trigger this func  */
+  /*   Set the  */
+
+  handleButtonPress = (id) => {
+    const date = new Date().getTime();
+    this.setState({ ...this.state, Time_stamp: date });
+  };
+
+  
+  /*  on mouse up button to record time diffrenence */ 
+  
   handleButtonRelease = (id) => {
-    clearTimeout(this.buttonPressTimer);
-    this.setState({ ...this.setState, timerid: null });
+    /*   condiction for 0 and 1 keys  */
     if (id === "0" || id === "1") {
-      var te = { ...this.state };
-      te.text = te.text.concat(id);
-      this.setState(te);
+      const state_copy = { ...this.state };
+      state_copy.text = state_copy.text.concat(id);
+      state_copy.timerid = null;
+      state_copy.date = null;
+      state_copy.id = -1;
+      this.setState(state_copy);
       return;
     }
 
-    var d = new Date();
-    var n = d.getTime();
-    if (n - this.state.date < 800) {
-    
+
+    /*   Difference between the mouse up and mouse down    */
+
+    const currTime = new Date().getTime();
+    if (currTime - this.state.Time_stamp < 800) {
       if (this.state.timerid) {
-        //   if timer exits
-        clearTimeout(this.state.timerid);
     
-        if (id === this.state.id) {
-          var str = { ...this.state };
-          str.text = str.text.substring(0, str.text.length - 1);
+    
+   /*  Previous timer exits */      
+        clearTimeout(this.state.timerid);
 
-          var iter = this.state.no;
-            if(!(id==='7'||id==='9')){
-          if (iter === 2) {
-            iter = 0;
+        if (id === this.state.key_id) {
+
+        /* Same Key is pressed again frequently */
+
+          const stateCopy = { ...this.state };
+
+          stateCopy.text = stateCopy.text.substring(
+            0,
+            stateCopy.text.length - 1
+          );
+
+          let arr_pos =this.state.arr_no;
+          
+          /* Exception for 7 and 9 key , unless all revert back to 0 cyclically */
+          if (!(id === "7" || id === "9")) {
+            if (arr_pos === 2) {
+              arr_pos = 0;
+            } else {
+              arr_pos = arr_pos + 1;
+            }
           } else {
-            iter = iter + 1;
+            if (arr_pos === 3) {
+              arr_pos = 0;
+            } else {
+              arr_pos = arr_pos + 1;
+            }
           }
-        }else{
-            if (iter === 3) {
-                iter = 0;
-              } else {
-                iter = iter + 1;
-              }
-            
-        }
 
-          str.no = iter;
-          str.text = str.text.concat(keys[id][iter]);
+          stateCopy.arr_no = arr_pos;
 
-          var time = this.timercall();
-          str.timerid = time;
-          this.setState(str);
-        } 
-        else {
-          var str = { ...this.state };
-          str.text = str.text.concat(keys[id][0]);
-          str.id = id;
-          str.no = 0;
-          var time = this.timercall();
-          str.timerid = time;
+          stateCopy.text = stateCopy.text.concat(keys[id][arr_pos]);
 
-          this.setState(str);
+          const time = this.timercall();
+          stateCopy.timerid = time;
+          this.setState(stateCopy);
+        } else {
+
+            /*   Different Key is pressed frequently */
+          const stateCopy = { ...this.state };
+          stateCopy.text = stateCopy.text.concat(keys[id][0]);
+          stateCopy.key_id = id;
+          stateCopy.arr_no = 0;
+          const time = this.timercall();
+          stateCopy.timerid = time;
+          this.setState(stateCopy);
         }
       } else {
-    
-        var tim = this.timercall();
 
-        var t = { ...this.state };
-        t.text = t.text.concat(keys[id][0]);
-    t.id = id;
-        t.no = 0;
-        t.timerid = tim;
-        this.setState(t);
+        /*   if previous timer is not there  */
+        const timer = this.timercall();
+
+        const stateCopy = { ...this.state };
+        stateCopy.text = stateCopy.text.concat(keys[id][0]);
+        stateCopy.key_id = id;
+        stateCopy.arr_no = 0;
+        stateCopy.timerid = timer;
+        this.setState(stateCopy);
       }
     } else {
-      var copy = { ...this.state };
-      copy.text = copy.text.concat(id.toString());
-      copy.id = 1;
-      copy.no = 0;
-      copy.timerid = tim;
-      this.setState(copy);
+
+      /*   Long press of key to show button   */
+      const stateCopy = { ...this.state };
+      stateCopy.text = stateCopy.text.concat(id.toString());
+      stateCopy.key_id = -1;
+      stateCopy.arr_no = 0;
+      stateCopy.timerid = null;
+      this.setState(stateCopy);
     }
   };
 
+
+
+/* Timer function  */
+
   timercall = () => {
-    var time = setTimeout(() => {
-      var st = { ...this.state };
-      st.id = 1;
-      st.no = 0;
-      st.timerid = null;
-      this.setState(st);
+    const timer = setTimeout(() => {
+      const stateCopy = { ...this.state };
+      stateCopy.key_id = -1;
+      stateCopy.arr_no = 0;
+      stateCopy.timerid = null;
+      this.setState(stateCopy);
     }, 2000);
-    return time;
+    return timer;
   };
 
+
+
+  /*   To remove the rightmost alphabet on screen */
   remove = () => {
     clearTimeout(this.state.timerid);
-    let str = { ...this.state };
-    let len = str.text.length;
-    str.text = str.text.substring(0, len - 1);
-    str.timerid = null;
-    this.setState(str);
+    const stateCopy = { ...this.state };
+    const len = stateCopy.text.length;
+    stateCopy.text = stateCopy.text.substring(0, len - 1);
+    stateCopy.timerid = null;
+    this.setState(stateCopy);
   };
 
+
+  /*   To clear all screen */
   clear = () => {
-    this.setState({ ...this.state, text: "" });
+    this.setState({
+      ...this.state,
+      text: "",
+      key_id: -1,
+      arr_no: 0,
+      timerid: null,
+      Time_stamp: null,
+    });
   };
 
   render() {
-    console.log(" re-rendering ");
     return (
-      <div
-        style={{
-          width: "100%",
-          textAlign: "center",
-          margin: "auto",
-          display: "block",
-        }}
-      >
-        <div
-          style={{
-            height: "400px",
-            width: "350px",
-            textAlign: "center",
-            margin: "auto",
-            border: "2px red solid",
-            "backgroundColor":"#FFFF00"
-          }}
-        >
+      <div>
+        <Div2>
           <h2 className="mt-3">Keypad</h2>
-          <textarea className="mt-2" readOnly style={{resize: "none"}} value={this.state.text} />
+          <textarea
+          rows="1" cols="27"
+            className="mt-2"
+            readOnly
+            style={{ resize: "none" }}
+            value={this.state.text}
+          />
           <div></div>
-
-          <div className="row pl-5 pt-3">
-            {Object.entries(keys).map((entry) => (
-              <div key={entry[0]}  className="col-3" style={{ margin: "5px" }}>
-                <button
-                  sttle={{backgroundColor:"red"}}
-                  className="btn btn-primary btn-xg btn-block" style={{paddingRight:"50px",textAlign:"center"}}
-
-                  onTouchEnd={() => this.handleButtonRelease(entry[0])}
-                  onMouseDown={this.handleButtonPress}
-                  onMouseUp={() => this.handleButtonRelease(entry[0])}
-                >
-                  <div>{entry.map((item) => item)}</div>
-                </button>
-              </div>
-            ))}
-            <button
-              className="btn btn-primary ml-4" key="10"
-              onClick={() => {
-                this.remove();
-              }}
-            >
-              <div>back</div>
-            </button>
-
-            <button
-              className="btn btn-primary ml-4" key="11"
-              onClick={() => {
-                this.clear();
-              }}
-            >
-              <div>Clear</div>
-            </button>
-          </div>
-
-        </div>
+           
+        <Button clear={this.clear} back={this.remove} 
+        mousedown={(id)=>{this.handleButtonPress(id)}}
+         mouseup={(id)=>{this.handleButtonRelease(id)}}   />            
+          
+        </Div2>
       </div>
     );
   }
 }
+
+
+
+const Div2=styled.div`
+height: 400px;
+width: 350px;
+textAlign: center;
+margin: auto;
+border: 2px red solid;
+background: #FFFF00;
+
+`;
